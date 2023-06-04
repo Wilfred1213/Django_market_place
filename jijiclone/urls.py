21de79f54d7b4c95d2476e2e2bc38d0a47d91cd0
Wilfred1213 <mathiaswilfred7@gmail.com>
2023-06-04 14:38:58 +0100

    newly updated

--- jijiclone/urls.py
+++ jijiclone/urls.py
@@ -24,5 +24,7 @@ urlpatterns = [
         template_name='jijiapp/registration/password_reset_complete.html'), name='password_reset_complete'),
 
 ]
-if settings.DEBUG:
-    urlpatterns +=static(settings.MEDIA_URL, document_root =settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# if settings.DEBUG:
+#     urlpatterns +=static(settings.MEDIA_URL, document_root =settings.MEDIA_ROOT)
